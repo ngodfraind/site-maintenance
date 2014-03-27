9c85e2b440e0521284797de5eb408b1f01d9864a
ngodfraind <nicolas.godfraind@gmail.com>
2014-03-27 16:32:28 +0100

    Adding permissions.php.

--- permissions.php
+++ permissions.php
@@ -1,4 +1,14 @@
 <?php 
 
-const CONFIG_FILE = '/home/jorge/documents/backup/config-test.php';
+const CONFIG_FILE = '/path/to/config.php';
+const PERMISSION_FILE = '/path/to/permissions.sh';
+
+$config = include CONFIG_FILE;
+
+foreach ($config['sites'] as $site) {
+	$folderName = array_keys($site)[0];
+	system("sh " . PERMISSION_FILE . " {$site['owner']} {$site[$folderName]['path']}");
+}
+
+
 
